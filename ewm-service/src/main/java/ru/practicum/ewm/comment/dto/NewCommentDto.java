257bd54614956d2d48b4f1577fad0abdbffb650c
Denis <deniska_ledenev@mail.ru>
2026-07-01 22:47:53 +0300

    remove unnecessary @Builder from small DTOs and classes

--- ewm-service/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
@@ -3,12 +3,10 @@ package ru.practicum.ewm.comment.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class NewCommentDto {
--- ewm-service/src/main/java/ru/practicum/ewm/location/dto/LocationDto.java
+++ ewm-service/src/main/java/ru/practicum/ewm/location/dto/LocationDto.java
@@ -2,12 +2,10 @@ package ru.practicum.ewm.location.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class LocationDto {
